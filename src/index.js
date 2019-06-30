ab2519d153018a3894977ac515ae21f39fcfc33c
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2019-06-30 10:45:06 -0700

    rewrite the command and add tests

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import algolia from './algolia'
-
-hexo.extend.console.register('algolia', 'Index your posts on Algolia', {
-  options: [{
-    name: '-n, --no-clear', desc: 'Does not clear the existing index'
-  }]
-}, algolia)
---
+++ test_blog/source/_posts/hello-world.md
@@ -0,0 +1,4 @@
+---
+title: Hello World
+---
+This is a test post
